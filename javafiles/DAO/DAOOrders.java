274fdff2774d923cb0465a9b4b404c35edff43a0
ivan <ivanquintans_@outlook.es>
2024-05-09 13:11:24 +0200

    chore: DAO orders done

--- javafiles/DAO/DAOOrders.java
+++ javafiles/DAO/DAOOrders.java
@@ -1,5 +1,66 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DAOOrders {
+    private static final String nombre_tabla = "orders";
+
+    //añadimos un order a la base de datos
+    public boolean addOrder(String userEmail, Float orderAmount) {
+        //intentamos la conexión con la base de datos
+        try (Connection conexion = ConnectionManager.getConnection()) {
+           // Creamos la consulta SQL parametrizada
+            String sql = "INSERT INTO " + nombre_tabla + " (user_email, amount) VALUES (?, ?)";
+            try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
+                // Establecemos los parámetros de la consulta
+                stmt.setString(1, userEmail);
+                stmt.setFloat(2, orderAmount);
+                // Ejecutamos la consulta
+                int filasInsertadas = stmt.executeUpdate();
+                // Comprobamos si se insertó correctamente
+                if (filasInsertadas > 0) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error en la ejecución de la sentencia" + e.getMessage());
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    //Mostramos al usuario el valor del precio total de su pedido
+
+    public Integer showOrder(String email) { //con el email del usuario obtenemos el pedido con indice mas alto 
+        Integer orderamount = 0;
+         //intentamos la conexión con la base de datos
+         try (Connection conexion = ConnectionManager.getConnection()) {
+           // Creamos la consulta SQL parametrizada para evitar posibles ataques de inyección SQL
+            String sql = "SELECT amount FROM " + nombre_tabla + " WHERE email = ? AND id = (SELECT MAX(id) FROM " + nombre_tabla + " WHERE email = ?)";
+            try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
+                stmt.setString(1, email);
+                stmt.setString(2, email);
+                
+                // Ejecutamos la consulta
+                ResultSet rs = stmt.executeQuery();
+                // Si se encuentra algún resultado
+                if (rs.next()) {
+                    // Obtenemos el valor de amount de la fila
+                    String amountString = rs.getString("amount");
+                    orderamount = Integer.parseInt(amountString);
+                }
+            }
     
+        } catch (SQLException e) {
+            System.err.println("Error en la ejecución de la sentencia" + e.getMessage());
+            e.printStackTrace();
+        }
+        return orderamount;
+       
+    }
 }
+    
